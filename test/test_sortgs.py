643dd55b104c3acfc829577ec43f456a229aec0c
Fernando Wittmann <fernando.wittmann@gmail.com>
2021-03-14 18:31:06 -0300

    Add Unit Testing In order to make contributions, all of the tests must be passed. In order to test the code, we will be using the DEBUG mode which is going to use a URL from web archive. Please make sure to save the URL you want to test on web archive in case it is different from the one I already saved. By default it only works in debug mode when using the keywords 'machine learning' There are 6 tests and all of them are testing different aspects that should match when using SortGS.

---
+++ test/test_sortgs.py
@@ -0,0 +1,53 @@
+import unittest
+from unittest.mock import patch
+import sortgs
+import os
+import pandas as pd
+
+class TestSortGS(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        '''run once before all tests'''
+        os.system("python sortgs.py --debug --kw 'machine learning' --nresults 10")
+        self.df_top_10=pd.read_csv('machine_learning.csv')
+
+        os.system("python sortgs.py --debug --kw 'machine learning' --nresults 20")
+        self.df_top_20=pd.read_csv('machine_learning.csv')
+
+        os.system("python sortgs.py --debug --kw 'machine learning' --nresults 20 --sortby 'cit/year'")
+        self.df_top_sorted_cit_per_year=pd.read_csv('machine_learning.csv')
+    
+    def test_get_10_results(self):
+        self.assertEqual(len(self.df_top_10), 12) # Two extra unwanted elements were captured
+    
+    def test_get_20_results(self):
+        self.assertEqual(len(self.df_top_20), 23) # Three extra unwanted elements were captured
+    
+    def test_is_sorted(self):
+        df=self.df_top_20
+        top_citations=list(df.Citations.values[:5])
+        self.assertEqual(top_citations, [49230, 8603, 3166, 3069, 2853])
+    
+    def test_top_result(self):
+        df=self.df_top_20
+        top_author = str(df.Author.values[0])
+        top_citation = int(df.Citations.values[0])
+        top_cit_per_year = int(df['cit/year'].values[0])
+        top_results = [top_author, top_citation, top_cit_per_year]
+        self.assertEqual(top_results, [' Bishop', 49230, 3077])
+
+    def test_cit_per_year_sorted(self):
+        df=self.df_top_sorted_cit_per_year
+        top_citations=list(df.Citations.values[:5])
+        top_cit_per_year = list(df['cit/year'].values[:5])
+        top_results = [top_citations, top_cit_per_year]
+        self.assertEqual(top_results, [[49230, 8603, 2853, 3166, 2416],
+                                        [3077,  860,  713,  396,  345]])
+
+    def test_csv_exists(self):
+        os.system("python sortgs.py --debug --kw 'machine learning' --nresults 10")
+        self.assertTrue(os.path.exists('machine_learning.csv'))
+
+
+if __name__=='__main__':
+    unittest.main()
